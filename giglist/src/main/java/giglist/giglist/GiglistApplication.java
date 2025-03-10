1f04f0c3f00ec1d1c74eca2ff721ac372243f03a
ElliPiilonen <ellipiilonen@gmail.com>
2025-03-10 11:35:03 +0200

    starting the project

---
+++ giglist/src/main/java/giglist/giglist/GiglistApplication.java
@@ -0,0 +1,13 @@
+package giglist.giglist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiglistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiglistApplication.class, args);
+	}
+
+}
---
+++ giglist/src/test/java/giglist/giglist/GiglistApplicationTests.java
@@ -0,0 +1,13 @@
+package giglist.giglist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiglistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
